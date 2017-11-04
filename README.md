78c86185888080e1ce68158b1e4fccdf0459ccce
skyguyver <lynardsalingujay@yahoo.com>
2017-11-05 02:07:45 +0800

    solved the acct_no sequence

--- README.md
+++ README.md
@@ -22,3 +22,4 @@ docker run -v /dev_path/odoo/mp_odoo11/trunk:/mnt/extra-addons -v /dev_path/odoo
 # copy files from container
 docker cp malphi:/usr/lib/python3/dist-packages/odoo /dev_path/github/mp_odoo11
 docker cp /lynard.ebglobal/development/odoo/11/addons/base/ir/ir_sequence.py malphi:/usr/lib/python3/dist-packages/odoo/addons/base/ir/
+docker cp D:/dev/github/mp_odoo11/addons/base/ir/ir_sequence.py malphi:/usr/lib/python3/dist-packages/odoo/addons/base/ir/
--- mp_base/model/customer.py
+++ mp_base/model/customer.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 from odoo.exceptions import Warning
 
 _logger = logging.getLogger(__name__)
@@ -30,9 +30,17 @@ class customers(models.Model):
 class customer_acct(models.Model):
     _name = 'mp.customer_acct'
 
+    @api.model
+    def create(self, vals):
+        if vals.get('acct_no', _('New')) == _('New'):
+            vals['acct_no'] = self.env['ir.sequence'].next_by_code('customer_acct.acctno.sequence') or _('New')
+        result = super(customer_acct, self).create(vals)
+        return result
+
     name = fields.Char(related='customer_id.name', string='Customer')
-    acct_no = fields.Char(size=20, string='Account number', index=True, default=lambda self: self.env['ir.sequence'].next_by_code('customer_acct.acctno.sequence'))
-    passport = fields.Char()
+    acct_no = fields.Char(size=20, string='Account number', required=True, copy=False, readonly=True, index=True, default=lambda self: _('New'))
+    passport = fields.Char(size=20, string='Passport')
+    passport_expiry = fields.Date(string='Expiry')
     date_activated = fields.Date(string='Date activated')
     customer_id = fields.Many2one(comodel_name='mp.customer', string='Customer', ondelete='restrict')
     comments = fields.Char()
